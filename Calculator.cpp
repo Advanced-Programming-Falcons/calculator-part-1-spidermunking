302eb33d06eca4c72d3d063d774798f0c56889d0
spidermunking <105009002+spidermunking@users.noreply.github.com>
2024-08-26 11:49:52 -0600

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -10,31 +10,34 @@ int loop = 1;
 
 int main(void){
     while(loop == 1){
-        cout << "Please enter a number" << endl;
-        cin >> num;
+        if (num3 == 0){
+            cout << "Please enter a number" << endl;
+            cin >> num;
+            num3 = 1
         // user enters a number
-        cout << "Please enter an opperator ( +, -, *, /, **)" << endl;
+        }
+        cout << "Please enter an opperator ( +, -, *, /, ^)" << endl;
         cin >> op;
         // user enters an operator
         cout << "please enter another number to use with the operator" << endl;
         cin >> num2;
         // user enters another number to use with operator
         if (op == '+') {
-            num3 = num + num2;
+            num = num + num2;
         }
         else if (op == '-'){
-            num3 = num - num2;
+            num = num - num2;
         }
         else if (op == '*'){
-            num3 = num * num2;
+            num = num * num2;
         }
         else if (op == '/'){
-            num3 = num / num2;
+            num = num / num2;
         }
-        else if (op == '**'){
-            num3 = pow(num,num2);
+        else if (op == '^'){
+            num = pow(num,num2);
         }
-        cout << num3 << endl;
+        cout << num << endl;
 
         // below should be at the end of the program so it can stop or loop again if the user wants. 
         cout << "Do you want to do another calculation?(1 for yes, 0 for no)" << endl;
